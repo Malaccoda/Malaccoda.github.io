61e313fa97b5b7e31a8a0fa1f425ec9c63b00c26
malaclypse <malaccoda@top-email.net>
2021-11-28 03:21:31 +0100

    Nikola auto commit.
    Nikola version: 8.1.3

--- cache/.mako.tmp/post.tmpl.py
+++ cache/.mako.tmp/post.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1638065603.6754825
+_modified_time = 1638066090.7847776
 _enable_loop = True
 _template_filename = 'themes/monospace/templates/post.tmpl'
 _template_uri = 'post.tmpl'
@@ -41,16 +41,16 @@ def render_body(context,**pageargs):
         __M_locals = __M_dict_builtin(pageargs=pageargs)
         def content():
             return render_content(context._locals(__M_locals))
-        _link = context.get('_link', UNDEFINED)
-        post = context.get('post', UNDEFINED)
         messages = context.get('messages', UNDEFINED)
+        helper = _mako_get_namespace(context, 'helper')
+        date_format = context.get('date_format', UNDEFINED)
+        pheader = _mako_get_namespace(context, 'pheader')
+        _link = context.get('_link', UNDEFINED)
         def extra_head():
             return render_extra_head(context._locals(__M_locals))
         comments = _mako_get_namespace(context, 'comments')
-        pheader = _mako_get_namespace(context, 'pheader')
         math = _mako_get_namespace(context, 'math')
-        date_format = context.get('date_format', UNDEFINED)
-        helper = _mako_get_namespace(context, 'helper')
+        post = context.get('post', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n')
         __M_writer('\n')
@@ -98,16 +98,16 @@ def render_extra_head(context,**pageargs):
 def render_content(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        helper = _mako_get_namespace(context, 'helper')
         def content():
             return render_content(context)
-        post = context.get('post', UNDEFINED)
         messages = context.get('messages', UNDEFINED)
-        comments = _mako_get_namespace(context, 'comments')
-        pheader = _mako_get_namespace(context, 'pheader')
-        math = _mako_get_namespace(context, 'math')
         date_format = context.get('date_format', UNDEFINED)
+        helper = _mako_get_namespace(context, 'helper')
+        pheader = _mako_get_namespace(context, 'pheader')
         _link = context.get('_link', UNDEFINED)
+        comments = _mako_get_namespace(context, 'comments')
+        math = _mako_get_namespace(context, 'math')
+        post = context.get('post', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n    <div class="post">\n    ')
         __M_writer(str(pheader.html_title()))
--- cache/.mako.tmp/tags.tmpl.py
+++ cache/.mako.tmp/tags.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1638065603.942135
+_modified_time = 1638066090.7027822
 _enable_loop = True
 _template_filename = 'themes/bootstrap/templates/tags.tmpl'
 _template_uri = 'tags.tmpl'
@@ -32,12 +32,12 @@ def render_body(context,**pageargs):
         def content():
             return render_content(context._locals(__M_locals))
         messages = context.get('messages', UNDEFINED)
-        items = context.get('items', UNDEFINED)
+        cat_items = context.get('cat_items', UNDEFINED)
+        range = context.get('range', UNDEFINED)
+        title = context.get('title', UNDEFINED)
         hidden_tags = context.get('hidden_tags', UNDEFINED)
         len = context.get('len', UNDEFINED)
-        range = context.get('range', UNDEFINED)
-        cat_items = context.get('cat_items', UNDEFINED)
-        title = context.get('title', UNDEFINED)
+        items = context.get('items', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
@@ -57,12 +57,12 @@ def render_content(context,**pageargs):
         def content():
             return render_content(context)
         messages = context.get('messages', UNDEFINED)
-        items = context.get('items', UNDEFINED)
+        cat_items = context.get('cat_items', UNDEFINED)
+        range = context.get('range', UNDEFINED)
+        title = context.get('title', UNDEFINED)
         hidden_tags = context.get('hidden_tags', UNDEFINED)
         len = context.get('len', UNDEFINED)
-        range = context.get('range', UNDEFINED)
-        cat_items = context.get('cat_items', UNDEFINED)
-        title = context.get('title', UNDEFINED)
+        items = context.get('items', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n<h1>')
         __M_writer(str(title))
